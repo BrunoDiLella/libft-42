8373fe42c420482ec88fc4cd7d051869180852de
Bruno Gonçalves <brgoncalves@hotmail.com.br>
2022-04-08 16:47:22 -0300

    ok

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdi-lell <bdi-lell@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 16:42:56 by bdi-lell          #+#    #+#             */
+/*   Updated: 2022/04/08 16:43:35 by bdi-lell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+    
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char*)(s + i) == (unsigned char)c)
+			return ((void*)(s + i));
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdi-lell <bdi-lell@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 16:40:23 by bdi-lell          #+#    #+#             */
+/*   Updated: 2022/04/08 16:41:56 by bdi-lell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int  cmp_char(char c1, char c2)
+{
+	if ((unsigned char)c1 != (unsigned char)c2)
+		return ((unsigned char)c1 - (unsigned char)c2);
+	return (0);
+}
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (cmp_char(s1[i], s2[i]))
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (i < n)
+		return (cmp_char(s1[i], s2[i]));
+	return (0);
+}
